5bb78cd60f6e7445d85da136abc8d05fcae0a416
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2013-01-27 13:06:42 -0500

    Fixed naming of planks

--- node_defs.lua
+++ node_defs.lua
@@ -63,7 +63,7 @@ for i in ipairs(trees) do
 	})
 
 	minetest.register_node("moretrees:"..treename.."_planks", {
-		description = treedesc.." Beech Planks",
+		description = treedesc.." Planks",
 		tiles = {"moretrees_"..treename.."_wood.png"},
 		is_ground_content = true,
 		groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3,wood=1},
